e93d9c7ac5eaeb346214564752283d3662301116
mathador <niehoff.mathias@gmail.com>
2024-11-04 21:08:14 +0100

    libraries and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'product',
+    title: 'product',
+    loadComponent: () => import('@nx/product').then((c) => c.ProductComponent),
+  },
+  //   {
+  //     path: 'shared',
+  //     loadComponent: () => import('@nx/shared').then((c) => c.SharedComponent),
+  //   },
+  {
+    path: '**', // 404
+    redirectTo: '',
+  },
+];
